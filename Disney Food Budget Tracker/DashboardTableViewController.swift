e3995162195f40df74a6e1bfc8f465570df7182c
Jack <doherty9@purdue.edu>
2018-06-29 17:52:45 -0400

    added VC for trip VC and started prep for segue in dash

--- Disney Food Budget Tracker/DashboardTableViewController.swift
+++ Disney Food Budget Tracker/DashboardTableViewController.swift
@@ -59,14 +59,10 @@ class DashboardTableViewController: UITableViewController {
         return cell
     }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        let destination = segue.destination as! TripViewController
+        
+        
     }
-    */
 
 }
