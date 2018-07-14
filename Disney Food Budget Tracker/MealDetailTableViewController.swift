f2138d4b966502680fa010d106138ab7a30291f4
Jack <doherty9@purdue.edu>
2018-07-14 13:43:28 -0400

    working on meal details

--- Disney Food Budget Tracker/MealDetailTableViewController.swift
+++ Disney Food Budget Tracker/MealDetailTableViewController.swift
@@ -7,17 +7,36 @@
 //
 
 import UIKit
+import FirebaseDatabase
+import FirebaseStorage
 
 class MealDetailTableViewController: UITableViewController {
+    
+    let tripID: String!
+    let dateString: String!
+    let mealString: String!
+    let mealsAndCosts: Dictionary = [String:Double]()
+    let mealsAndReceiptIDs: Dictionary = [String:String]()
+    
+    let ref = Database.database().reference()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
+        ref.child("trips/\(tripID!)/dates/\(dateString)/\(mealString)").observeSingleEvent(of: .value, with: { (snapshot) in
+            let value = snapshot.value as? NSDictionary
+            
+            for keys in value! {
+                let entryID = keys.key
+                let cost = keys.value["cost"] as Double
+                let receipt = keys.value["receipt"] as? String ?? nil
+                
+                mealsAndCosts = [entryID:cost]
+                mealsAndReceiptIDs = [entryID:receipt]
+            }
+        }) { (error) in
+            print(error.localizedDescription)
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -29,58 +48,32 @@ class MealDetailTableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return mealsAndCosts.count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
+        let cell = tableView.dequeueReusableCell(withIdentifier: "detailCell", for: indexPath)
+        let mealsAndCostsArr = Array(mealsAndCosts).sorted(by: <)
+        
+        cell.textLabel?.text = mealsAndCostsArr[indexPath.row].key
+        cell.detailTextLabel?.text = String(format: "%.2f", mealsAndCostsArr[indexPath.row].value)
 
         return cell
     }
-    */
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
+//    // Override to support editing the table view.
+//    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+//        if editingStyle == .delete {
+//            // Delete the row from the data source
+//            tableView.deleteRows(at: [indexPath], with: .fade)
+//        } else if editingStyle == .insert {
+//            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+//        }
+//    }
 
     /*
     // MARK: - Navigation
